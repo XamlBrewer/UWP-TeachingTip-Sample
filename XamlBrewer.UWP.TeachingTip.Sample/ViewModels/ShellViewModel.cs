05569dfec0a4286483442f9a6307318efdd5e97e
Diederik Krols <diederik_krols@hotmail.com>
2019-08-06 12:30:56 +0200

    Add project files.

---
+++ XamlBrewer.UWP.TeachingTip.Sample/ViewModels/ShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Mvvm.Services;
+using XamlBrewer.Uwp.TeachingTip.Sample;
+
+namespace Mvvm
+{
+    internal class ShellViewModel : ViewModelBase
+    {
+        public ShellViewModel()
+        {
+            // Build the menus
+            Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("HomeIcon"), Text = "Home", NavigationDestination = typeof(HomePage) });
+            Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("CalloutIcon"), Text = "Main", NavigationDestination = typeof(MainPage) });
+            SecondMenu.Add(new MenuItem() { Glyph = Icon.GetIcon("InfoIcon"), Text = "About", NavigationDestination = typeof(AboutPage) });
+        }
+    }
+}
